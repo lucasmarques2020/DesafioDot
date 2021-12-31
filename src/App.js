050596655371c2dfd2e3700c6133afad23979699
lucasmarques2020 <luquinhas.mb07@gmail.com>
2021-12-31 13:02:33 -0300

    Branch padrao para desenvolver outra features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import logo from './logo.svg';
+
 import './App.css';
-import TelaInicial from './components/telainicial/TelaInicial'
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <TelaInicial/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          OLá Lucas Marques Bandeira
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mais informações
-        </a>
-      </header>
-    </div>
-  );
+    <></>
+  )
 }
 
-export default App;
--- src/components/telainicial/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export const NavBarBK = styled.div`
-    background-color: red;
-
-`
