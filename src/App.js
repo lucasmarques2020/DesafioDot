23bf791af5b76ce4079705fcc197511dae67e9a7
lucasmarques2020 <luquinhas.mb07@gmail.com>
2021-12-31 17:34:04 -0300

    Criaçao do componente de Cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Cards from './components/cards/Cards'
 
 export default function App() {
   return (
-    <></>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='d-flex flex-wrap justify-content-center p-5'>
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+        </div>
+      </div>
+    </div>
   )
 }
 
